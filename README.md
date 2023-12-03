819507a02cd7f5f89857927c27ba36e58dd0b3be
givanz <givanz@yahoo.com>
2023-12-04 01:19:11 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
-# [Templatecookie](https://templatecookie.com)
-Templatecookie.com creates quality templates and php scripts. Templatecookie has many free HTML & Figma templates available for professional use. Templatecookie is famous for its premium PHP Scripts available on [Codeanyon Marketplace](https://codecanyon.net/user/templatecookie). Browse [Templatecookie](https://templatecookie.com) today and discover awesome digital products.
-
-# [Poco HTML Agency Landing Page Template](https://www.templatecookie.com/templates/varin-html-template)
+# [Bd Weather Free Bootstrap 5 Comming Soon Template]
 
 > Poco is a beautiful landing page template for agencies, It has many pages including, services, about, contact, portfolio, portfolio details, blog, blog details, and more.
 
---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require('gulp');
+const fileinclude = require('gulp-file-include');
+//const sass = require('gulp-sass')(require('sass'));
+const sass = require('gulp-sass')(require('node-sass'));
+const formatHtml = require('gulp-format-html');
+const through2 = require( 'through2' );    
+
+const touch = () => through2.obj( function( file, enc, cb ) {
+    if ( file.stat ) {
+        file.stat.atime = file.stat.mtime = file.stat.ctime = new Date();
+    }
+    cb( null, file );
+});
+
+gulp.task('fileinclude', function() {
+  return gulp.src(['./src/*.html', './src/**/*.html', '!**/_*/**'])
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(formatHtml())
+    .pipe( touch() )
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('sass', function() {
+  return gulp.src(['./scss/*.scss'])
+    .pipe(sass())
+    .pipe(gulp.dest('./css'));
+});
+
+
+gulp.task('watch', function () {
+    gulp.watch(['./src/*.html', './src/**/*.html'], gulp.series('fileinclude'));
+    gulp.watch(['./scss/*.scss'], gulp.series('sass'));
+});
+
+
+// Default Task
+gulp.task('default', gulp.series('fileinclude', 'sass'));
